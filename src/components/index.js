02772a0af2768d8c43091b9a9423d9d237efc5d1
Akshat7garg <akshat7garg@gmail.com>
2025-11-08 19:23:20 +0530

    chore: initialize component file structure

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { Footer } from "./custom/Footer";
+import { Header } from "./custom/Header";
+import { Loader } from "./custom/Loader";
+import { Deck } from "./sections/Deck";
+import { Highlights } from "./sections/Highlights";
+import { Log } from "./sections/Log";
+import { Manual } from "./sections/Manual";
+
+export {
+    Deck,
+    Footer,
+    Header,
+    Highlights,
+    Loader,
+    Log,
+    Manual
+};
---
+++ src/components/sections/Manual.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Manual = () => {
+  return (
+    <div>Manual</div>
+  )
+}
